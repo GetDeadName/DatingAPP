b434b50e11d5289a17f5717b3a0067e6ff5c1d23
GetDeadName <irene.monedero@getronics.com>
2022-10-05 14:53:27 +0200

    13th part

---
+++ API/Entities/UserLike.cs
@@ -0,0 +1,12 @@
+using API.entities;
+
+namespace API.Entities
+{
+    public class UserLike
+    {
+        public AppUser SourceUser { get; set; }
+        public int SourceUserId { get; set; }
+        public AppUser LikedUser { get; set; }
+        public int LikedUserId { get; set; }
+    }
+}
